590dc79e5e168f27dcbdc02fec29f0924e62974b
darthpasta <liowj.nick@gmail.com>
2017-05-01 14:38:59 +0800

    Completed challenge 120

--- Challenge120/ar-student-schema-master/app/models/student.rb
+++ Challenge120/ar-student-schema-master/app/models/student.rb
@@ -12,5 +12,11 @@ attr_accessor :first_name, :last_name, :age
 		age = Date.today.year - birthday.year
     	age -= 1 if Date.today < birthday + age.years
 	end
+
+	validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+	validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }        
+	validates :age, numericality: {only_integer: true, greater_than: 5}
+	validates :phone, format: { with: /\(\d{3}\)\s\d{3}-\d{4}\s[a-z]\d{4}/}
+
 end
 
